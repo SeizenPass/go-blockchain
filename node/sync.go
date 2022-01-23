5cf712018cc4f7483ccf734b15583fd6552657ba
miras alimov <sektor.miras@gmail.com>
2022-01-24 03:10:58 +0600

    fixed: syncing

--- node/sync.go
+++ node/sync.go
@@ -12,7 +12,7 @@ func (n *Node) sync(ctx context.Context) error {
 
 	for {
 		select {
-		case <- ticker.C:
+		case <-ticker.C:
 			fmt.Println("Searching for new Peers and Blocks...")
 
 			n.fetchNewBlocksAndPeers()
@@ -23,8 +23,8 @@ func (n *Node) sync(ctx context.Context) error {
 }
 
 func (n *Node) fetchNewBlocksAndPeers() {
-	for _, peer := range n.knownPeers {
-		status, err := queryPeerStatus(peer)
+	for _, knownPeer := range n.knownPeers {
+		status, err := queryPeerStatus(knownPeer)
 		if err != nil {
 			fmt.Printf("ERROR: %s\n", err)
 			continue
@@ -34,15 +34,15 @@ func (n *Node) fetchNewBlocksAndPeers() {
 		if localBlockNumber < status.Number {
 			newBlocksCount := status.Number - localBlockNumber
 
-			fmt.Printf("Found %d new blocks from Peer %s\n", newBlocksCount, peer.IP)
+			fmt.Printf("Found %d new blocks from Peer %s\n", newBlocksCount, knownPeer.IP)
 		}
 
-		for _, statusPeer := range status.KnownPeers {
-			newPeer, isKnownPeer := n.knownPeers[statusPeer.TcpAddress()]
+		for _, maybeNewPeer := range status.KnownPeers {
+			_, isKnownPeer := n.knownPeers[maybeNewPeer.TcpAddress()]
 			if !isKnownPeer {
-				fmt.Printf("Found new Peer %s\n", peer.TcpAddress())
+				fmt.Printf("Found new Peer %s\n", knownPeer.TcpAddress())
 
-				n.knownPeers[statusPeer.TcpAddress()] = newPeer
+				n.knownPeers[maybeNewPeer.TcpAddress()] = maybeNewPeer
 			}
 		}
 	}
