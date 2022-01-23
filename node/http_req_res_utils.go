12414d4f2cefa8f5404ae582d6776b396fa4868e
miras alimov <sektor.miras@gmail.com>
2022-01-24 02:45:10 +0600

    added: peerNode struct

---
+++ node/http_req_res_utils.go
@@ -0,0 +1,42 @@
+package node
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func writeErrRes(w http.ResponseWriter, err error) {
+	jsonErrRes, _ := json.Marshal(ErrRes{err.Error()})
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write(jsonErrRes)
+}
+
+func writeRes(w http.ResponseWriter, content interface{}) {
+	contentJson, err := json.Marshal(content)
+	if err != nil {
+		writeRes(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(contentJson)
+}
+
+func readReq(r *http.Request, reqBody interface{}) error {
+	reqBodyJson, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("unable to read request body. %s", err.Error())
+	}
+	defer r.Body.Close()
+
+	err = json.Unmarshal(reqBodyJson, reqBody)
+	if err != nil {
+		return fmt.Errorf("unable to unmarshall request body. %s", err.Error())
+	}
+
+	return nil
+}
