1db4f4fe49b749038ae090ac97fd4ce41e3326e2
miras alimov <sektor.miras@gmail.com>
2022-05-22 19:10:56 +0600

    added: test to explain sig params

--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -4,15 +4,43 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/rand"
+	"fmt"
 	"github.com/SeizenPass/go-blockchain/database"
 	"github.com/SeizenPass/go-blockchain/fs"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"io/ioutil"
+	"math/big"
 	"testing"
 )
 
 const testKeystoreAccountsPwd = "security123"
+
+func TestSignCryptoParams(t *testing.T) {
+	privKey, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+	spew.Dump(privKey)
+
+	msg := []byte("Amiran and Beknur are my teammates")
+
+	sig, err := Sign(msg, privKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(sig) != crypto.SignatureLength {
+		t.Fatal(fmt.Errorf("wrong size for signature: got %d, want %d", len(sig), crypto.SignatureLength))
+	}
+
+	r := new(big.Int).SetBytes(sig[:32])
+	s := new(big.Int).SetBytes(sig[32:64])
+	v := new(big.Int).SetBytes([]byte{sig[64]})
+
+	spew.Dump(r, s, v)
+}
 
 func TestSign(t *testing.T) {
 	privKey, err := ecdsa.GenerateKey(crypto.S256(), rand.Reader)
